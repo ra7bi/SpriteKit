beadfb47dee2fc3b31b5b913e7cf82450309e7d3
fahad alrahbi <Fahad@fahads-MacBook-Pro.local>
2014-12-28 23:33:23 +0400

    First App with SpriteKit

--- SpriteKitDemo2/GameScene.m
+++ SpriteKitDemo2/GameScene.m
@@ -2,45 +2,76 @@
 //  GameScene.m
 //  SpriteKitDemo2
 //
-//  Created by fahad alrahbi on 28/12/14.
+//  Created by fa/Users/Fahad/Downloads/ball.pnghad alrahbi on 28/12/14.
 //  Copyright (c) 2014 fahad alrahbi. All rights reserved.
 //
 
 #import "GameScene.h"
 
 @implementation GameScene
+-(id)initWithSize:(CGSize)size {
+    if (self = [super initWithSize:size]) {
+        /* Setup your scene here */
+        
+    }
+    
+    return self;
+}
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    self.backgroundColor=[SKColor whiteColor];
     
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
+    // Pysic body to the scene
+    self.physicsBody =[SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
     
-    [self addChild:myLabel];
+    
+    
+    /*============= CGVectorMake(X,Y) ==========
+                  +y
+                 |
+                 |
+        -x ------|------ +x
+                 |
+                 |
+                  -Y
+     */
+    
+    
+    // change gravity settings of the physics world
+    self.physicsWorld.gravity = CGVectorMake(0,0);
+    
+    
+    // create new sprite node from an image
+    SKSpriteNode * ball =[SKSpriteNode spriteNodeWithImageNamed:@"ball"];
+    
+    
+    // create a CGPoint for position ( center of Scene )
+    //Get Screen center ( i'm getting center position )  الطول تقسيم ٢ و العرض تقسيم ٢
+    CGPoint myPoint =CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    ball.position = myPoint;
+    
+    
+    // Add physics body to the ball
+    ball.physicsBody =[SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+    
+    
+     // add the sprite to the scene
+    [self addChild:ball];
+    
+    
+    // Create the Vector
+    
+    CGVector myVector = CGVectorMake(20, 20);
+    
+    //Apply the vector to the ball
+    
+    [ball.physicsBody applyImpulse:myVector];
+   
+
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    /* Called when a touch begins */
-    
-    for (UITouch *touch in touches) {
-        CGPoint location = [touch locationInNode:self];
-        
-        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-        
-        sprite.xScale = 0.5;
-        sprite.yScale = 0.5;
-        sprite.position = location;
-        
-        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-        
-        [sprite runAction:[SKAction repeatActionForever:action]];
-        
-        [self addChild:sprite];
-    }
-}
+
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
